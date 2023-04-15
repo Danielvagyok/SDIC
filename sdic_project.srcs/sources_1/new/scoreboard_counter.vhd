50e3e03a06c3feee3ca85d869a768aa427d2f7b2
Daniel Matyas <matyasdaniel004@gmail.com>
2023-04-15 16:06:33 +0300

    Added block diagram and state things.
    Signed-off-by: Daniel Matyas <matyasdaniel004@gmail.com>

--- sdic_project.srcs/sources_1/new/scoreboard_counter.vhd
+++ sdic_project.srcs/sources_1/new/scoreboard_counter.vhd
@@ -36,13 +36,53 @@ entity scoreboard_counter is
            dec : in STD_LOGIC;
            clk : in STD_LOGIC;
            rst : in STD_LOGIC;
-           d1 : out STD_LOGIC_VECTOR (7 downto 0);
-           d0 : out STD_LOGIC_VECTOR (7 downto 0));
+           seg : out STD_LOGIC_VECTOR (0 to 6);
+           sel : out STD_LOGIC_VECTOR (3 downto 0));
 end scoreboard_counter;
 
 architecture Behavioral of scoreboard_counter is
 
+type states is (start, idle, count_down, count_up);
+signal current_state, next_state : states;
+
+component driver7seg is
+    Port ( clk : in STD_LOGIC; --100MHz board clock input
+           Din : in STD_LOGIC_VECTOR (15 downto 0); --16 bit binary data for 4 displays
+           an : out STD_LOGIC_VECTOR (3 downto 0); --anode outputs selecting individual displays 3 to 0
+           seg : out STD_LOGIC_VECTOR (0 to 6); -- cathode outputs for selecting LED-s in each display
+           dp_in : in STD_LOGIC_VECTOR (3 downto 0); --decimal point input values
+           dp_out : out STD_LOGIC; --selected decimal point sent to cathodes
+           rst : in STD_LOGIC); --global reset
+end component driver7seg;
+
 begin
 
+process(rst, clk)
+begin
+  if rst = '1' then
+    current_state <= start;
+  elsif rising_edge(clk) then
+    current_state <= next_state;  
+  end if;
+end process;
+
+process(current_state, inc, dec)
+begin
+    case current_state is
+        when start => next_state <= idle;
+        when idle => if (inc xor dec) = '0' then
+                        next_state <= idle;
+                     elsif inc = '1' then
+                        next_state <= count_up;
+                     elsif dec = '1' then
+                        next_state <= count_down;
+                     else
+                        next_state <= start;
+                     end if;
+        when count_up => next_state <= idle;
+        when count_down => next_state <= idle;
+        when others => next_state <= start;
+    end case;
+end process;
 
 end Behavioral;
