1ea0b4b70bc3b7e69bda95beb80990dc9d929914
Daniel Matyas <matyasdaniel004@gmail.com>
2023-05-23 04:18:32 +0300

    Scoreboard counter.
    Left button = increment
    Right button = decrement
    Up button = change player
    Point = 1 + number of switches turned on
    
    The first 2 digits of the 7seg display correspond to the first plyer,
    the last 2 correspond to the second player.
    
    The buttons were tested, the point modification is yet to be tested.
    
    TODO: Stop at a certain point: maybe 99 upper limit and 0 lower limit.
    
    Further work ideas: Allow the user to set the upper limit, e.g., for volleyball 25, for ping-pong 21, etc.
    
    Signed-off-by: Daniel Matyas <matyasdaniel004@gmail.com>

--- sdic_project.srcs/sources_1/new/scoreboard_counter.vhd
+++ sdic_project.srcs/sources_1/new/scoreboard_counter.vhd
@@ -34,8 +34,10 @@ use IEEE.NUMERIC_STD.ALL;
 entity scoreboard_counter is
     Port ( inc : in STD_LOGIC;
            dec : in STD_LOGIC;
+           ch : in STD_LOGIC;
            clk : in STD_LOGIC;
            rst : in STD_LOGIC;
+           sw : in std_logic_vector (15 downto 0);
            seg : out STD_LOGIC_VECTOR (0 to 6);
            sel : out STD_LOGIC_VECTOR (3 downto 0);
            dp : out std_logic);
@@ -43,10 +45,13 @@ end scoreboard_counter;
 
 architecture Behavioral of scoreboard_counter is
 
-type states is (start, idle, count_down, count_up);
+type states is (start, idle, playerSel, count_down, count_up);
 signal current_state, next_state : states;
 
 signal score: std_logic_vector (15 downto 0);
+signal player: std_logic := '0';
+
+shared variable point : integer range 1 to 17;
 
 component driver7seg is
     Port ( clk : in STD_LOGIC; --100MHz board clock input
@@ -64,12 +69,14 @@ component debounce is
            btnDeb : out STD_LOGIC);
 end component;
 
-signal incDeb, decDeb : std_logic;
+signal incDeb, decDeb, chDeb : std_logic;
 
 begin
 
 deb1 : debounce port map (clk => clk, btn => inc, btnDeb => incDeb);
 deb2 : debounce port map (clk => clk, btn => dec, btnDeb => decDeb);
+deb3 : debounce port map (clk => clk, btn => ch, btnDeb => chDeb);
+
 
 ff: process(rst, clk)
 begin
@@ -99,47 +106,152 @@ begin
     end case;
 end process;
 
+player_select: process(rst, clk)
+begin
+    if rst = '1' then
+        player <= '0';
+    elsif rising_edge(clk) then
+        if current_state = idle and chDeb = '1' then
+            case player is
+                when '0' => player <= '1';
+                when '1' => player <= '0';
+                when others => player <= 'X';
+            end case;
+        end if;
+    end if;
+end process;
+
+point_select: process(clk)
+begin
+    if rising_edge(clk) then
+        point := 1;
+        for i in 0 to 15 loop
+            if sw(i) = '1' then
+                point := point + 1;
+            end if;
+        end loop;
+    end if;
+end process;
+
 count: process(rst, clk)
 --variable thousand : integer range 0 to 9 := 0;
 --  variable hundred : integer range 0 to 9 := 0;
-  variable ten : integer range 0 to 9 := 0;
-  variable unit : integer range 0 to 9 := 0;
+  variable ten1 : integer range 0 to 9 := 0;
+  variable ten2 : integer range 0 to 9 := 0;
+  variable unit1 : integer range 0 to 9 := 0;
+  variable unit2 : integer range 0 to 9 := 0;
+  variable carry : integer range 0 to 2;
+  variable borrow : integer range 0 to 2;
 begin
   if rst = '1' then
 --    thousand := 0;
 --    hundred := 0;
-    ten := 0;
-    unit := 0;
+    ten1 := 0;
+    ten2 := 0;
+    unit1 := 0;
+    unit2 := 0;
   elsif rising_edge(clk) then
-    if current_state = count_up then
-      if unit = 9  then
-        unit := 0;
-        if ten = 9 then
-          ten := 0;
-        else  
-          ten := ten + 1;
-        end if;  
-      else
-        unit := unit + 1;
-      end if;
-    elsif current_state = count_down then
-        if unit = 0 then
-            unit := 9;
-            if ten = 0 then 
-              ten := 9;
+    if player = '0' then
+        if current_state = count_up then
+          if unit1 + point > 9  then
+            if unit1 + point - 10 > 9 then
+                carry := 2;
+            else
+                carry := 1;
+            end if;
+                     
+            case carry is
+                when 1 => unit1 := unit1 + point - 10;
+                when 2 => unit1 := unit1 + point - 20;
+            end case;
+            
+            if ten1 + carry > 9 then
+              ten1 := 0;
             else  
-              ten := ten - 1;
-            end if;  
-        else
-            unit := unit - 1;
+              ten1 := ten1 + carry;
+            end if;
+              
+          else
+            unit1 := unit1 + point;
+          end if;
+          
+        elsif current_state = count_down then
+           if unit1 - point < 0  then
+            if unit1 - point + 10 < 0 then
+                borrow := 2;
+            else
+                borrow := 1;
+            end if;
+                     
+            case borrow is
+                when 1 => unit1 := unit1 - point + 10;
+                when 2 => unit1 := unit1 - point + 20;
+            end case;
+            
+            if ten1 - borrow < 0 then
+              ten1 := 9;
+            else  
+              ten1 := ten1 - borrow;
+            end if;
+              
+          else
+            unit1 := unit1 - point;
+          end if;
         end if;
-    end if;
+     elsif player = '1' then
+         if current_state = count_up then
+          if unit2 + point > 9  then
+            if unit2 + point - 10 > 9 then
+                carry := 2;
+            else
+                carry := 1;
+            end if;
+                     
+            case carry is
+                when 1 => unit2 := unit2 + point - 10;
+                when 2 => unit2 := unit2 + point - 20;
+            end case;
+            
+            if ten2 + carry > 9 then
+              ten2 := 0;
+            else  
+              ten2 := ten2 + carry;
+            end if;
+              
+          else
+            unit2 := unit2 + point;
+          end if;
+          
+        elsif current_state = count_down then
+           if unit2 - point < 0  then
+            if unit2 - point + 10 < 0 then
+                borrow := 2;
+            else
+                borrow := 1;
+            end if;
+                     
+            case borrow is
+                when 1 => unit2 := unit2 - point + 10;
+                when 2 => unit2 := unit2 - point + 20;
+            end case;
+            
+            if ten2 - borrow < 0 then
+              ten2 := 9;
+            else  
+              ten2 := ten2 - borrow;
+            end if;
+              
+          else
+            unit2 := unit2 - point;
+          end if;
+        end if;
+     end if;
   end if;  
   
-  score <= std_logic_vector(to_unsigned(15,4)) &
-           std_logic_vector(to_unsigned(15,4)) &
-           std_logic_vector(to_unsigned(ten,4)) &
-           std_logic_vector(to_unsigned(unit,4));
+  score <= std_logic_vector(to_unsigned(ten2,4)) &
+           std_logic_vector(to_unsigned(unit2,4)) &
+           std_logic_vector(to_unsigned(ten1,4)) &
+           std_logic_vector(to_unsigned(unit1,4));
   
 end process;
 
@@ -147,7 +259,7 @@ display_score: driver7seg port map (clk => clk,
                              Din => score,
                              an => sel,
                              seg => seg,
-                             dp_in => (others =>'0'),
+                             dp_in => "0100",
                              dp_out => dp,
                              rst => rst);
 
